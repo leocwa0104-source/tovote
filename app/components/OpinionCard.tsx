f55f1e3ab01ee1cabd2a108bf7f0039f2f71f69c
garfline <leocwa0104@gmail.com>
2026-02-27 08:53:38 +0800

    fix-missing-useRef-import

--- app/components/OpinionCard.tsx
+++ app/components/OpinionCard.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { createOpinion, deleteOpinion } from '@/app/actions'
 import OpinionDetailModal from './OpinionDetailModal'
 import MentionTextarea from './MentionTextarea'
