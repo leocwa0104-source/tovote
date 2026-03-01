0e9faf34d81899bed327246615c28bf29d4500f3
garfline <leocwa0104@gmail.com>
2026-03-01 20:28:46 +0800

    fix:_add_missing_useEffect_import_in_OpinionCard

--- app/components/OpinionCard.tsx
+++ app/components/OpinionCard.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { createOpinion, deleteOpinion } from '@/app/actions'
 import OpinionDetailModal from './OpinionDetailModal'
 import MentionTextarea from './MentionTextarea'
